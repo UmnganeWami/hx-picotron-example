ec4f5a6cf7d4b9f7f1f0c87e8501a5ef0e68b8e3
Umngane Wami <82868738+UmnganeWami@users.noreply.github.com>
2024-03-23 21:04:09 +1000

    everything else

---
+++ src/Main.hx
@@ -0,0 +1,23 @@
+package;
+
+import picotron.Window;
+import picotron.Picotron;
+
+class Main {
+	static function main() {
+		new Main();
+	}
+
+	public function new() {
+		Picotron._update = Update;
+		Picotron._draw = Draw;
+		Window.makeWindow(250, 150, "Hello Title!");
+	}
+
+	function Draw() {
+		Picotron.cls(0);
+		Picotron.print("Hello World!", 0, 8);
+	}
+
+	function Update() {}
+}
